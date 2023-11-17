6e2e9a58762ca5a0f84efca17c1d2f837040ff32
Jason Shang <jshang27@cchairHXJKFD3S265D.local>
2023-11-16 19:09:31 -0500

    moved all april tag code to one file and fixed distance_to_pixels function

--- cchs_vision/april_tag_detection.py
+++ cchs_vision/april_tag_detection.py
@@ -1,82 +1,58 @@
 import cv2
-from cv2 import aruco
 import numpy as np
 import yaml
 
-count = 0
+
 class Detector:
-
-
-    def __init__(self,cfg):
+    def __init__(self, cfg):
         if cfg["marker_family"] == "16h5":
-            self.aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_APRILTAG_16H5)
+            self.aruco_dict = cv2.aruco.getPredefinedDictionary(
+                cv2.aruco.DICT_APRILTAG_16H5)
         else:
-            raise Exception("Unknown Family!")
-        self.parameters = aruco.DetectorParameters()
+            raise ValueError("Unknown Family!")
+        self.parameters = cv2.aruco.DetectorParameters()
+
     def detect(self, img):
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray,
-          self.aruco_dict, None, parameters=self.parameters)
-        if not ids:
+        grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        corners, ids, _ = cv2.aruco.detectMarkers(
+            grayscale_img, self.aruco_dict, None, parameters=self.parameters)
+        if ids == None:
             return None
-        markers = [corners, ids]
-        #
-        # markers = []
-        #
-        # for i, c in zip(ids, corners):
-        #     #mark = {"id": i, "xrad":0.23, "yrad":0.1, "d":2}
-        #     mark = {"id": i}
-        #     markers.append(mark)
-        #
-        #     (topLeft, topRight, bottomRight, bottomLeft) = c.reshape((4, 2))
-        #
-        #     print(c.reshape((4, 2)))
-        #
-        #     topRight = (int(topRight[0]), int(topRight[1]))
-        #     bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
-        #     bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
-        #     topLeft = (int(topLeft[0]), int(topLeft[1]))
-        return markers
+        return [corners, ids]
 
 
+def distance_to_pixels(pixel_length, height, vert_fov_deg, april_tag_size_in):
+    theta = np.radians(vert_fov_deg)
+    theta_prime = pixel_length / height * theta
+    return april_tag_size_in / np.tan(theta_prime)
 
 
-
-
-
-
-
-
-
-if __name__ == '__main__' :
+if __name__ == '__main__':
     with open("config.yaml", "r") as file:
         cfg = yaml.safe_load(file)
     det = Detector(cfg)
     cap = cv2.VideoCapture(cfg["camera_id"])
     if not cap.isOpened():
-        print("err")
-        quit()
-
+        print("unable to open camera")
+        quit(-1)
     cap.set(3, cfg["width"])
     cap.set(4, cfg["height"])
+
     while True:
         ret, img = cap.read()
         markers = det.detect(img)
-        print(markers)
-        # for m in markers:
-        #     for c in m["corners":
-        #
         if markers:
             corners, ids = markers
-            frame_markers = cv2.aruco.drawDetectedMarkers(img, corners, ids)
-        # plt.savefig("_data/markers.pdf")
-        # plt.show()
-
-        cv2.imshow("markers", img)
-
-
-
+            frame_markers = cv2.aruco.drawDetectedMarkers(
+                img.copy(), corners, ids)
+            for i in corners:
+                (topLeft, topRight, bottomRight, bottomLeft) = i.reshape((4, 2))
+                leftLength = topLeft[1] - bottomLeft[1]
+                rightLength = topRight[1] - bottomRight[1]
+                bottomLength = bottomRight[0] - bottomLeft[0]
+                topLength = topRight[0] - topLeft[0]
+                dist = distance_to_pixels(
+                    (leftLength + rightLength) / 2, cfg["height"], cfg["vert_fov"], cfg["marker_size_in"])
+                print(f"{dist} Meters")
         if cv2.waitKey(1) == ord('q'):
             break
-   # [{"id":12, }]
-
