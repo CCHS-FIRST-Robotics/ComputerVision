852bd26d5765340edaa61f5afcc5464bb04a2371
ColinBalfour <75389927+ColinBalfour@users.noreply.github.com>
2024-10-19 19:35:02 -0400

    aruco works

--- src/marker.py
+++ src/marker.py
@@ -1,5 +1,3 @@
-from multiprocessing import Semaphore, Value, shared_memory
-
 import cv2
 import numpy as np
 
@@ -9,14 +7,35 @@ from utils import get_dim, get_shm_frame
 def marker_detect(cfg, shm, sem, procid, quit):
 
     cam = cfg["camera"]
+    mark = cfg["marker"]
+
     tw, th = get_dim(cam["w"], cam["h"], cam["wr"])
+
+    if mark["family"] == "36h11":
+        marker_dict = cv2.aruco.DICT_APRILTAG_36h11
+    else:
+        print("unknown marker", mark["family"] )
+        return
+
+    dictionary = cv2.aruco.getPredefinedDictionary(marker_dict)
+    detectorparams = cv2.aruco.DetectorParameters()
+    detector = cv2.aruco.ArucoDetector(dictionary, detectorparams)
 
     while True:
         frame = get_shm_frame(shm, sem, (th, tw, cam["c"]))
-        # sem.acquire()
-        # shm_array = np.ndarray(shape=(th, tw, cam["c"]), dtype=np.uint8, buffer=shm.buf)
-        # frame = np.copy(shm_array)
-        # sem.release()
+
+        corners, markerids, rejects = detector.detectMarkers(frame)
+        #markers = []
+        cv2.aruco.drawDetectedMarkers(frame, corners, markerids)
+
+        if markerids is not None:
+            for c, id in zip(corners, markerids):
+                c = c.squeeze()
+                cx = int(c[:,0].sum() / 4)
+                cy = int(c[:,1].sum() / 4)
+                #markers.append({"id":id, "c":(cx, cy)})
+
+                cv2.circle(frame, (cx, cy), 4, (0, 0, 255), -1)
 
         cv2.imshow("marker det " + str(procid), frame)
 
