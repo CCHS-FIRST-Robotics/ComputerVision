2e88e1b024dd66a5f89108766042d604c17658c1
Istvan Sleder <istvan.sleder@gmail.com>
2025-02-09 23:25:17 -0500

    black

--- sample_code/networktables/cli.py
+++ sample_code/networktables/cli.py
@@ -5,7 +5,7 @@ import ntcore
 from ntcore import NetworkTableInstance
 
 # Replace" localhost" with the robot’s IP if remote
-#IP = "10.32.5.2"
+# IP = "10.32.5.2"
 IP = "localhost"
 
 # Create a NetworkTables instance
@@ -13,9 +13,9 @@ ntinst = NetworkTableInstance.getDefault()
 ntinst.startClient4("jetson")  # Start client with name "jetson"
 
 # Connect to the server (Replace with correct IP if needed)
-ntinst.setServer(IP) 
+ntinst.setServer(IP)
 # print default port
-print("PORT",ntcore.NetworkTableInstance.kDefaultPort4)
+print("PORT", ntcore.NetworkTableInstance.kDefaultPort4)
 
 # Get the tag table
 sd = ntinst.getTable("tags")
@@ -23,16 +23,18 @@ sd = ntinst.getTable("tags")
 # send data to server
 print("Client started, waiting for data...")
 
+
 def make_tags():
-    n = random.randint(1,5)
+    n = random.randint(1, 5)
     tags = []
 
     for i in range(n):
-        id = random.randint(1,22)
+        id = random.randint(1, 22)
         angle = random.random() * 360 - 180
         d = random.random() * 10
-        tags.extend([i,angle,d])
+        tags.extend([i, angle, d])
     return tags
+
 
 i = 0
 try:
@@ -42,10 +44,9 @@ try:
         data = make_tags()
         data.insert(0, i)
         print(data)
-        sd.putNumberArray("tags",data)
-        
+        sd.putNumberArray("tags", data)
+
         i += 1
         time.sleep(1)  # update every second
 except KeyboardInterrupt:
     print("Shutting down client.")
-
--- sample_code/networktables/srv.py
+++ sample_code/networktables/srv.py
@@ -1,4 +1,4 @@
-# This is a NetworkTables server demo 
+# This is a NetworkTables server demo
 import time
 
 import ntcore
@@ -12,7 +12,7 @@ ntinst.setServerTeam(TEAM)
 ntinst.startServer()  # Start server on default port 1735
 
 # print default port
-print("PORT",ntcore.NetworkTableInstance.kDefaultPort4)
+print("PORT", ntcore.NetworkTableInstance.kDefaultPort4)
 
 # Create a table named "tags"
 sd = ntinst.getTable("tags")
@@ -26,12 +26,12 @@ try:
         tags = sd.getNumberArray("tags", None)
         if tags is not None:
             print(tags)
-            print("packetid",tags[0])
-            n = (len(tags)-1) // 3
+            print("packetid", tags[0])
+            n = (len(tags) - 1) // 3
             print("count", n)
             for i in range(n):
                 ix = i * 3 + 1
-                print(tags[ix:ix+3])
+                print(tags[ix : ix + 3])
         print(".")
         time.sleep(1)  # Update every second
 except KeyboardInterrupt:
