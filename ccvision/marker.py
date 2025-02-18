220043ecdfc373861ca29a793b94a65fc5bf7321
Istvan Sleder <istvan.sleder@gmail.com>
2025-02-17 19:07:02 -0500

    adding calc angle, networktables

--- ccvision/marker.py
+++ ccvision/marker.py
@@ -3,6 +3,8 @@ import time
 import cv2
 import numpy as np
 
+from .angles import Angles
+from .NetworkTable import NetworkTable
 from .utils import get_dim, get_shm_frame
 
 
@@ -24,35 +26,50 @@ def marker_detect(cfg, shm, sem, procid, quit):
     detectorparams = cv2.aruco.DetectorParameters()
     detector = cv2.aruco.ArucoDetector(dictionary, detectorparams)
 
-    # the 4 cameras are combined into a wide image 400x2560
-    imw = cfg["camera"]["wr"] // 4  # one camera width
+    angles = Angles(cam["imw"], th, cam["fovh"])
+
+    # Create a NetworkTables instance
+    network = NetworkTable(cfg, "tags")
+    packetid = 0
 
     p_tm = time.time()
     while True:
         frame = get_shm_frame(shm, sem, (th, tw, cam["c"]))
         frames = {}
-        markers = {}
+        markers = []
 
         # Do imarker detection only cameras specified in cfg
         for i in cfg["marker"]["cameraids"]:
-            framei = frame[:, i * imw : (i + 1) * imw, :]
+            framei = frame[:, i * cam["imw"] : (i + 1) * cam["imw"], :]
+            framei = cv2.cvtColor(framei, cv2.COLOR_RGB2GRAY)
             corners, markerids, rejects = detector.detectMarkers(framei)
-            markers[i] = []
 
             # Calculate and draw center point
             if markerids is not None:
-                for c, id in zip(corners, markerids):
+                for c, id in zip(corners, markerids):  # corners, markerid
                     c = c.squeeze()
                     cx = int(c[:, 0].sum() / 4)
                     cy = int(c[:, 1].sum() / 4)
-                    markers[i].append((id, cx, cy))
 
-                if cfg["display"]["marker"]:
-                    cv2.circle(framei, (cx, cy), 4, (0, 0, 255), -1)
+                    # TODO change to use sides
+                    pixel_sz = c[:, 0].max() - c[:, 0].min()
+
+                    angleh_rad, anglev_rad = angles.get_angle(cx, cy)
+                    dist = angles.get_distance(pixel_sz, cfg["marker"]["size"])
+
+                    pose = 0
+                    markers.extend([id, pose, angleh_rad, anglev_rad, dist])
+
+                    if cfg["display"]["marker"]:
+                        cv2.circle(framei, (cx, cy), 4, (0, 0, 255), -1)
 
             if cfg["display"]["marker"]:
                 cv2.aruco.drawDetectedMarkers(framei, corners, markerids)
                 frames[i] = framei
+
+        if len(markers) > 0:
+            network.send_array("tags", markers.insert(0, packetid))
+            packetid += 1
 
         if cfg["display"]["marker"]:
             iframe = np.hstack(list(frames.values()))
