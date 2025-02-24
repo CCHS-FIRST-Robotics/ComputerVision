d2fc840e65ed97b1b24949a1b1b934202b9ee77c
Istvan <istvan.sleder@gmail.com>
2025-02-23 23:29:12 -0500

    set frame ix

--- tools/slice.py
+++ tools/slice.py
@@ -1,14 +1,18 @@
-import cv2
 import os
+
+import cv2
 
 img_dir = "images"
 out_dir = "imgs_out"
+cam_ix = 2
 
 for row in os.scandir(img_dir):
 
     print(row.name)
     img = cv2.imread(row.path)
     w = img.shape[1] // 4
-    img = img[:,:w,:]
+    fr = cam_ix * w
+    to = (cam_ix + 1) * w
+    img = img[:, fr:to, :]
     cv2.imwrite(f"{out_dir}/{row.name}", img)
     print(img.shape)
