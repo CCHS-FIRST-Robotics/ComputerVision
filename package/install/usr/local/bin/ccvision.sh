2aec3ffb81e53e8f481a05eabda8ef45aeb9bdfe
Istvan <istvan.sleder@gmail.com>
2025-03-02 12:58:32 -0500

    removed

--- package/install/usr/local/bin/ccvision.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-dt=date
-echo "ccvision `$dt`" >> /var/log/ccvision.log
-
-su - jetson
-
-export PYTHONPATH=/home/jetson/ComputerVision
-python -m ccvision.main -c /home/jetson/ComputerVision/config.yaml
