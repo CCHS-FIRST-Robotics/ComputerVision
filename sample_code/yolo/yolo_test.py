16af23028c668d5889b1c70b4d0d16a2b5da4bce
Jonathan Li <jonathanli668899@gmail.com>
2025-04-06 09:47:13 -0400

    :U

--- sample_code/yolo/yolo_test.py
+++ sample_code/yolo/yolo_test.py
@@ -19,23 +19,20 @@ while True:
 
     for r in results:
         for b in r.boxes:
-            x1, y1, x2, y2 = b.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            x1, y1, x2, y2 = b.xyxy[0] #coordinates of bounding box
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) #convert to int coordinates
 
-            cls = int(b.cls[0])
-            conf = b.conf[0]
-            txt = f"{r.names[cls]} {conf:.2f}"
+            classID = int(b.cls[0])
+            confidence = b.confidence[0]
 
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 1)
+            txt = f"{r.names[classID]} {confidence:.2f}" #what is detected, & confidence rating
+
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 1)
             cv2.putText(img, txt, (x1, y1), font, fontScale, color, thickness)
 
-            # print("b conf",b.conf[0])
-            # print("cls",r.names[cls], b.conf[0])
+    cv2.imshow("frame", img)
 
-    cv2.imshow("Hello", img)
-
-    k = cv2.waitKey(1)
-    if k == 27:
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
